cd4607dbbe91185a29140451a4b63844fdefff06
Maxime_Rayvich <maximerayvich@gmail.com>
2022-11-08 13:36:16 -0500

    create bindings, create registerActivity

--- app/src/main/java/com/example/pokemongame/RegisterActivity.kt
+++ app/src/main/java/com/example/pokemongame/RegisterActivity.kt
@@ -4,19 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pokemongame.databinding.ActivityMainBinding
+import com.example.pokemongame.databinding.ActivityRegisterBinding
 
-private lateinit var binding: ActivityMainBinding
-class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityRegisterBinding
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        binding.start.setOnClickListener(){
-            val intent = Intent(this, RegisterActivity::class.java)
-        }
     }
 }
