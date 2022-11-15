645eb43a5fc2ac1188a5460e93a422b6ac0968ed
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-11-15 18:29:27 -0500

    changed pp frol val to var

--- app/src/main/java/com/example/pokemongame/pokemon/Move.kt
+++ app/src/main/java/com/example/pokemongame/pokemon/Move.kt
@@ -1,6 +1,6 @@
 package com.example.pokemongame.pokemon
 
 data class Move(
-    val name: String, val accuracy: Int, val maxPP: Int, val pp: Int = maxPP,
+    val name: String, val accuracy: Int, val maxPP: Int, var pp: Int = maxPP,
     val power: Int, val damageClass: String, val type: String,
     val target: String, val ailment: String? = null, val ailmentChance: Int) {}
