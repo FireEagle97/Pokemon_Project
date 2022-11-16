fd93f75c3759fda90cb3857eaeff41e8f5094def
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-11-15 20:13:37 -0500

    Continued damage calculation code, now up to type effectiveness

--- app/src/main/java/com/example/pokemongame/battle/DamageCalculations.kt
+++ app/src/main/java/com/example/pokemongame/battle/DamageCalculations.kt
@@ -1,8 +1,11 @@
 package com.example.pokemongame.battle
 
+import android.content.Context
+import com.example.pokemongame.JSONReader
 import com.example.pokemongame.pokemon.Move
 import com.example.pokemongame.pokemon.MoveAssigner
 import com.example.pokemongame.pokemon.Pokemon
+import com.google.gson.Gson
 import java.util.logging.Logger
 
 class DamageCalculations {
@@ -10,22 +13,36 @@ class DamageCalculations {
         val DamageLog: Logger = Logger.getLogger(DamageCalculations::class.java.name)
     }
     //If the move does no damage, determine it outside of this class
-    fun attack(attackerPokemon: Pokemon, attackerMove : Move, defenderPokemon: Pokemon){
+    fun calculateDamage(attackerPokemon: Pokemon, attackerMove : Move, defenderPokemon: Pokemon, context: Context): Int{
 
         //Attack hits Defence and Special Attack hits Special Defence
         var damage: Int = if(attackerMove.damageClass == "PHYSICAL"){
-            calculateDamage(attackerPokemon.level, attackerPokemon.Attack, attackerMove, defenderPokemon.Defence)
+            calculateInitialDamage(attackerPokemon.level, attackerPokemon.Attack, attackerMove, defenderPokemon.Defence)
         } else {
-            calculateDamage(attackerPokemon.level, attackerPokemon.SpecialAttack, attackerMove, defenderPokemon.SpecialDefence)
+            calculateInitialDamage(attackerPokemon.level, attackerPokemon.SpecialAttack, attackerMove, defenderPokemon.SpecialDefence)
         }
 
         //Calculate STAB bonus
         if(attackerMove.type in attackerPokemon.types){
-
+            DamageLog.info("STAB bonus applied!")
+            damage = kotlin.math.floor(damage * 1.5).toInt()
         }
+
+        //Type effectiveness
+        var multiplier: Double = 1.0
+        val gson = Gson()
+        var effectivenessJsonString = JSONReader().jSONReader(context, "type_relations/${attackerMove.type}.json")
+        var effectiveness = gson.fromJson(effectivenessJsonString, Effectiveness::class.java)
+        defenderPokemon.types.forEach {
+            if(effectiveness == null){
+
+            }
+        }
+
+        return damage
     }
 
-    private fun calculateDamage(attackerLevel: Int, attackerStat: Int, attackerMove: Move, defenderStat: Int): Int {
+    private fun calculateInitialDamage(attackerLevel: Int, attackerStat: Int, attackerMove: Move, defenderStat: Int): Int {
         val damage: Int = (((2 * attackerLevel)/5 + 2) / 50) * attackerMove.power * (attackerStat/defenderStat) + 2
         return damage
     }
---
+++ app/src/main/java/com/example/pokemongame/battle/Effectiveness.kt
@@ -0,0 +1,5 @@
+package com.example.pokemongame.battle
+
+data class Effectiveness (val normal: String?, val fire: String?, val water: String?, val electric: String?, val grass: String?,
+val ice: String?, val fighting: String?, val poison: String?, val ground: String?, val flying: String?, val psychic: String?,
+val bug: String?, val rock: String?, val ghost: String?, val dragon: String?)
