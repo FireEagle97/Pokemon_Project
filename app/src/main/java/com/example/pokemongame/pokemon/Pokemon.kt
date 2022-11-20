d4c8ea186e13aa0ac9c4e6194256b7d3a4d2b0cb
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-11-19 19:37:22 -0500

    pulling from staging

---
+++ app/src/main/java/com/example/pokemongame/pokemon/Pokemon.kt
@@ -0,0 +1,9 @@
+package com.example.pokemongame.pokemon
+
+data class Pokemon(
+                    val battleStats: BattleStats,
+                    val species : String,
+                    val name : String = species,
+                    val moves :
+
+)
