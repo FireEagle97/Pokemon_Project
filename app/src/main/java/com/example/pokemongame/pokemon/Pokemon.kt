baa4e7b04979361f69ac7af96ae20dd486ff5edb
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2022-11-19 19:56:21 -0500

    add Pokemon data class

--- app/src/main/java/com/example/pokemongame/pokemon/Pokemon.kt
+++ app/src/main/java/com/example/pokemongame/pokemon/Pokemon.kt
@@ -1,11 +1,15 @@
 package com.example.pokemongame.pokemon
 
-
+//experience type is temporary an int
 data class Pokemon(
                     val battleStats: BattleStats,
                     val species : String,
                     val name : String = species,
-                    val moves: MutableList<Move>
+                    val moves: MutableList<Move>,
+                    val experience : Int,
+                    val level : Int,
+                    val types : MutableList<String>,
+                    val hp : Int
 
 )
 
