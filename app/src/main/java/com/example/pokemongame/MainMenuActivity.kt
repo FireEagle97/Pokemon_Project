9ecd628df38aa95db8cdc355653dedf7c38a1b15
Maxime_rayvich <maximerayvich@gmail.com>
2022-11-22 00:17:30 -0500

    create intent for result in mainmenu

--- app/src/main/java/com/example/pokemongame/MainMenuActivity.kt
+++ app/src/main/java/com/example/pokemongame/MainMenuActivity.kt
@@ -12,31 +12,56 @@ private lateinit var binding: ActivityMainMenuBinding
 private lateinit var team: ArrayList<Pokemon>
 private lateinit var trainerName: String
 private lateinit var collection: ArrayList<Pokemon>
+private var reqCode: Int = 1234
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        team = intent.getSerializableExtra("team") as ArrayList<Pokemon> // I think we can assume team will never be empty.
-        trainerName = intent.getStringExtra("trainerName").toString() // Similarly, user will have to have set a trainer name to get to this screen.
-        collection = if(intent.hasExtra("collection")){
+        team =
+            intent.getSerializableExtra("team") as ArrayList<Pokemon> // I think we can assume team will never be empty.
+        trainerName = intent.getStringExtra("trainerName")
+            .toString() // Similarly, user will have to have set a trainer name to get to this screen.
+        collection = if (intent.hasExtra("collection")) {
             intent.getSerializableExtra("collection") as ArrayList<Pokemon>
-        } else{
+        } else {
             ArrayList<Pokemon>() // if not passed, create empty one
         }
 
 
-        binding.pokeCenterBtn.setOnClickListener{
+
+
+        binding.pokeCenterBtn.setOnClickListener {
 //            val intent = Intent(this, PokemonCenterActivity::class.java)
 //            startActivity(intent)
             //call a method to reset pp to maxPP and hp to maxHp
-            Toast.makeText(applicationContext, "Your pokemon team is ready to battle! ", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                applicationContext,
+                "Your pokemon team is ready to battle! ",
+                Toast.LENGTH_SHORT
+            ).show()
         }
-        binding.changeTeamBtn.setOnClickListener(){
+        //pass this data to activity where user swaps between team and collection
+        binding.changeTeamBtn.setOnClickListener() {
             val intent = Intent(this, TeamActivity::class.java)
-            startActivity(intent)
+            intent.putExtra("team", team)
+            intent.putExtra("collection", collection)
+            intent.putExtra("trainerName", trainerName)
+            startActivityForResult(intent, reqCode)
         }
 
+
+    }
+    // will need to check if need to do anyting in oncreate
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == reqCode) {
+                team = data!!.getSerializableExtra("team") as ArrayList<Pokemon>
+                collection = data.getSerializableExtra("collection") as ArrayList<Pokemon>
+                trainerName = data.getStringExtra("trainerName").toString()
+            }
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
