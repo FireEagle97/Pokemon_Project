9b0ee020a2f336e72687bf3f58ab3d2e97092c74
Maxime_rayvich <maximerayvich@gmail.com>
2022-11-21 23:03:03 -0500

    add creation of pokemon based on user input

--- app/src/main/java/com/example/pokemongame/RegisterActivity.kt
+++ app/src/main/java/com/example/pokemongame/RegisterActivity.kt
@@ -3,15 +3,10 @@ package com.example.pokemongame
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemongame.databinding.ActivityRegisterBinding
 import com.example.pokemongame.pokemon.Pokemon
 import com.example.pokemongame.pokemon.PokemonCreator
-import android.widget.RadioButton
-
-
-
 
 
 private lateinit var binding: ActivityRegisterBinding
@@ -34,8 +29,7 @@ class RegisterActivity : AppCompatActivity() {
         }
         binding.pokemon3.setCompoundDrawablesWithIntrinsicBounds(0,0,R.drawable.charmander,0)
 
-
-
+        //will store and pass pokemon here
         val team = ArrayList<Pokemon>()
         //when a pokemon is selected, allow user to select name and submit
         binding.firstChoice.setOnCheckedChangeListener { group, checkedId -> // I don't know if there is a way to remove these params
@@ -44,10 +38,30 @@ class RegisterActivity : AppCompatActivity() {
                 binding.menuBtn?.visibility = View.VISIBLE
             }
         binding.menuBtn?.setOnClickListener(){
-                if(binding.inputName.text.isNotBlank()){
-                    
-
+                if(binding.inputName.text.isNotBlank()) {
+                    //create selected pokemon
+                    val pokemonSpecies = arrayListOf<String>("bulbasaur", "squirtle", "charmander")
+                    val radioButtonID: Int = binding.firstChoice.checkedRadioButtonId
+                    val radioButton: View = binding.firstChoice.findViewById(radioButtonID)
+                    val ind: Int = binding.firstChoice.indexOfChild(radioButton)
+                    //set name based on if user provided one
+                    val name: String = if (binding.pokemonNameInput!!.text.isNotBlank()) {
+                        binding.pokemonNameInput!!.text.toString()
+                    } else {
+                        pokemonSpecies[ind]
                     }
+                    team.add(
+                        PokemonCreator().createPokemon(
+                            5,
+                            pokemonSpecies[ind],
+                            applicationContext,
+                            name
+                        )
+                    )
+
+                    val intent = Intent(this, MainMenuActivity::class.java)
+                    startActivity(intent)
+                }
             else{
                 binding.mustEnterName?.visibility  = View.VISIBLE
                 }
@@ -58,8 +72,7 @@ class RegisterActivity : AppCompatActivity() {
 
 //
 //        binding.menuBtn?.setOnClickListener{
-//            val intent = Intent(this, MainMenuActivity::class.java)
-//            startActivity(intent)
+
         //}
     }
 
