6292f84c8740401a16ae4bded3434ec7d505c0d5
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-12-08 20:41:18 -0500

    DamageCalculationsTest works but it takes a minute to execute :)

--- app/src/test/java/com/example/pokemongame/pokemon/LevelTest.kt
+++ app/src/test/java/com/example/pokemongame/pokemon/LevelTest.kt
@@ -1,14 +1,10 @@
 package com.example.pokemongame.pokemon
 
-import android.content.Context
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.jupiter.api.Assertions.*
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 internal class LevelTest {
-
-    var instrumentationContext: Context = InstrumentationRegistry.getInstrumentation().context
 
     @Test
     fun initializeLevels() {
