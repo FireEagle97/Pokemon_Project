82b4a92557192ad37c104fd28699f0994d469d5f
BastianFC0 <bastian.fernandezcortez@dawsoncollege.qc.ca>
2022-12-08 20:11:44 -0500

    Removed DamageCalculationTests due to being impossible to start the activity correctly

---
+++ app/src/androidTest/java/com/example/pokemongame/pokemon/LevelTest.kt
@@ -0,0 +1,24 @@
+package com.example.pokemongame.pokemon
+
+import android.content.Context
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.Test
+
+internal class LevelTest {
+
+    var instrumentationContext: Context = InstrumentationRegistry.getInstrumentation().context
+
+    @Test
+    fun initializeLevels() {
+    }
+
+    @Test
+    fun addExperience() {
+    }
+
+    @Test
+    fun levelUp() {
+    }
+}
--- app/src/test/java/com/example/pokemongame/pokemon/MoveAssignerTest.kt
+++ app/src/test/java/com/example/pokemongame/pokemon/MoveAssignerTest.kt
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test
 internal class MoveAssignerTest {
 
     @Test
+    //At level 5, squirtle learns 2 moves. We check if he has 2 moves
     fun assignNewMoves() {
+        val squirtle = PokemonCreator().createPokemon(5, "squirtle")
+        MoveAssigner().assignNewMoves(squirtle, squirtle.level)
+        assertEquals(2, squirtle.moves.count())
     }
 }
